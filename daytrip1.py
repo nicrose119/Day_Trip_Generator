1da6986943daf647d3e1bc2d730868187dcccdd1
nicrose119 <nicrose119@gmail.com>
2022-12-09 17:19:36 -0500

    Created initial random functions for each list in my  daytrip1.py

--- daytrip1.py
+++ daytrip1.py
@@ -1,1 +1,22 @@
+#user_satisfied = False
+
+import random
+destinations = ['New York City', 'Miami', 'Malibu', 'Aspen', 'Nashville']
+location = random.choice(destinations)   
+print(location)
+
+import random
+restaurants = ['Prime & Proper Steak House', 'Calexico', 'Slows BBQ', 'Island Eats', 'Bessa']
+restaurant = random.choice(restaurants)
+print(restaurant)
+
+import random
+mode_of_transportation = ['Plane', 'Train', 'Helicoptor', 'RV', 'Charter Bus']
+transportation = random.choice(mode_of_transportation)
+print(transportation)
+
+import random
+type_of_entertainment = ['Live Theatre', 'Concert', 'Day at the Spa', 'Guided Tour of citys attractions', 'Sky Diving']
+entertainment = random.choice(type_of_entertainment)
+print(entertainment)
 
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 #Single Responsibility Functions
-#4 Lists Destinations, restaurants, mode of transportation, entertainment
-#Values in lists will be strings
+
 #Random Selection for each list
 #Random Re-Selection of a value from a single list if user is not satisfied
     #Remove value from list if user is dissatisfied with that selection
     #Add a new value to list once a value is removed
 #while loop
+
+
+# (5 points): As a developer, I want to make at least three commits with descriptive messages 
+# (5 points):  As a developer, I want to store my destinations, restaurants, mode of transportation, and entertainment in their own separate Lists. 
+    #4 Lists Destinations, restaurants, mode of transportation, entertainment
+    #Values in lists will be strings
+# (5 points): As a user, I want a destination to be randomly selected for my day trip. 
+# (5 points): As a user, I want a restaurant to be randomly selected for my day trip
+# (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip. 
+# (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
+    #Random Selection for each list
+    
+# (15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things.
