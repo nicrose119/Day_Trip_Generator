eaf5525396d43615bc77aaf2e7ac2398cc957a77
nicrose119 <nicrose119@gmail.com>
2022-12-09 15:48:44 -0500

    Created Lists on my list.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#Single Responsibility Functions
+#4 Lists Destinations, restaurants, mode of transportation, entertainment
+#Values in lists will be strings
+#Random Selection for each list
+#Random Re-Selection of a value from a single list if user is not satisfied
+    #Remove value from list if user is dissatisfied with that selection
+    #Add a new value to list once a value is removed
+#while loop
