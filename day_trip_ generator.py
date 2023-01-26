c3d97c8fafb0b66f9add65856fd2aa4382b7784b
Nicole Overholt <nicrose119@gmail.com>
2023-01-26 13:25:38 -0500

    Corrected file name and project name name in git hub. Replaced spaces with underscores

---
+++ day_trip_ generator.py
@@ -0,0 +1,27 @@
+from lists import destinations, restaurants, mode_of_transportation, type_of_entertainment
+import random
+
+def generate_trip():
+    location = random.choice(destinations)
+    restaurant = random.choice(restaurants)
+    transportation = random.choice(mode_of_transportation)
+    entertainment = random.choice(type_of_entertainment)
+
+    trip = [location, restaurant, transportation, entertainment]
+    return trip
+
+def display_trip(selections_list):
+    for selection in selections_list:
+        print(selection)
+
+
+
+user_response = 'n'
+while user_response != 'y':
+    trip = generate_trip()
+    display_trip(trip)
+    user_response = input("Are you satisfied with your trip? y/n ") 
+    if user_response == 'y':
+        print("Here are the details of your final trip!")
+        display_trip(trip)
+
